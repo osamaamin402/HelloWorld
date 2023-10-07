87c44ae2e06eecb7fe0714a6f86a18cca69821f4
osama <osama@MSI>
2023-10-07 23:03:47 +0530

    Second commit

--- HelloWorld/src/main/java/com/oak/users/controller/HelloWorldController.java
+++ HelloWorld/src/main/java/com/oak/users/controller/HelloWorldController.java
@@ -1,5 +1,7 @@
 package com.oak.users.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,8 +9,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloWorldController {
 
 	@GetMapping
-	public String get() {
-		return "hello world";
+	public ResponseEntity<String> get() {
+		return new ResponseEntity<>("Hello World",HttpStatus.OK);
 	}
 	
 }
